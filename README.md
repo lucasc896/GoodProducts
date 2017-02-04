88faeb43987e1e64867b5313ec0dbea70519b5d4
Chris Lucas <lucasc896@gmail.com>
2017-02-04 18:15:18 +0000

    basic structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# GoodProducts
+
+This is going to be the best RESTful api ever for products and stuff.
---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import find_packages, setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "really_good_products_api",
+    version = "0.0.1",
+    author = "Chris Lucas",
+    author_email = "lucasc896@gmail.com",
+    description = ("RESTful API for the best products ever."),
+    packages=['tests'],
+    long_description=read('README'),
+    url="https://github.com/lucasc896/really_good_products_api",
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    entry_points={
+        # "console_scripts": [
+        #     "manage-db=handup.models.manage:main",
+        # ]
+    }
+)
